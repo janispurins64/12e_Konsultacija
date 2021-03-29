3677f5b56ea1872d1d7e36d1ceb06884d6fb499a
janispurins64 <janispurin64@gmail.com>
2021-03-29 15:31:11 +0300

    Kalkulators 2
    Reizināšana ir izveidota

--- js/f_12e.js
+++ js/f_12e.js
@@ -1,11 +1,24 @@
-function reiz(){
-  //  let in_x0 = document.getElementById("x0");
-  //  let vertiba = in_x0.getAttribute("value");
-    console.log("vertiba");
+function reiz(ra){
+  let in_x0 = document.getElementById("x0");
+  let xx = in_x0.textContent; 
+  in_x0.setAttribute("op1",xx);
+  in_x0.textContent = "0";
+  in_x0.setAttribute("op_kods",String(ra));
 }
+function rez(ra){
+  let in_x0 = document.getElementById("x0");
+  let oper2 = parseFloat(in_x0.textContent); 
+  let oper1 = parseFloat(in_x0.getAttribute("op1"));
+  oper1 = oper1 * oper2;
+  in_x0.textContent = String(oper1);
+}
+
 function cipars(aa){
     let in_x0 = document.getElementById("x0");
     let xx = in_x0.textContent;
-    in_x0.innerHTML = xx + aa;
-    console.log(xx);
+
+    in_x0.textContent = xx+String(aa);
+
+    //console.log(xx);
 }
+
